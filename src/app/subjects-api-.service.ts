782540ed40fa1a6260e704a1bc7610ed72f366e5
Jose Salamanca <jose.salamanca03@uptc.edu.co>
2023-05-28 21:42:20 -0500

    proyecto 1

--- src/app/subjects-api-.service.ts
+++ src/app/subjects-api-.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsApiService {
+readonly subjectAPIUrl="https://localhost:7243/api"
+  constructor(private http:HttpClient) { }
+  getInspectionList():Observable<any[]>{
+  return this.http.get<any>(this.subjectAPIUrl+'/Subjects');
 
-  constructor() { }
+  }
+  addInspection(data:any){
+  return this.http.post(this.subjectAPIUrl+'/Subjects', data);
+  }
+  updateInspection(id:number|String, data:any){
+  return this.http.put(this.subjectAPIUrl+`/Subjects/${id}`, data);
+  }
+
 }
